3e59f153e4c76fef0604b0770331368fa2bd2319
TonyDev77 <tony.music7@gmail.com>
2021-03-24 09:42:15 -0300

    Initialized Project

---
+++ src/main/java/com/tony/myvaccine/MyVaccineApplication.java
@@ -0,0 +1,13 @@
+package com.tony.myvaccine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVaccineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyVaccineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tony/myvaccine/MyVaccineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tony.myvaccine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyVaccineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
