dc38022806f8eec72533410f627502ab40bf6957
TonyDev77 <tony.music7@gmail.com>
2021-03-24 13:48:18 -0300

    Patient entity CRUD implemented

--- src/main/java/com/tony/myvaccine/resources/PatientResources.java
+++ src/main/java/com/tony/myvaccine/resources/PatientResources.java
@@ -3,11 +3,11 @@ package com.tony.myvaccine.resources;
 import com.tony.myvaccine.domain.Patient;
 import com.tony.myvaccine.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/patients")
@@ -19,5 +19,34 @@ public class PatientResources {
     @GetMapping
     public List<Patient> findAll() {
         return patientRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Patient findById(@PathVariable Long id) {
+        return patientRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public Patient savePatient(@RequestBody Patient patient) {
+        return patientRepository.save(patient);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Patient> updatePatient(@PathVariable("id") Long id, @RequestBody Patient patient) {
+
+        return patientRepository.findById(id).map(
+                x -> {
+                    x.setCpf(patient.getName());
+                    x.setCpf(patient.getCpf());
+                    x.setCpf(patient.getAge());
+                    x.setCpf(patient.getBirthDate());
+                    x.setCpf(patient.getPhone());
+                    x.setCpf(patient.getEmail());
+
+                    Patient updatePatient = patientRepository.save(patient);
+
+                    return ResponseEntity.ok().body(updatePatient);
+                }
+        ).orElse(ResponseEntity.notFound().build());
     }
 }
